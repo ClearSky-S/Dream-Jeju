191eb141ef7da5e65e68153911119f0ebbb5131a
Jang Junhyuck <ggogono2@gmail.com>
2021-08-16 14:48:59 +0900

    slideshow

---
+++ js/slideshow.js
@@ -0,0 +1,44 @@
+let slides = document.querySelectorAll("#slides > img");
+let prev = document.getElementById("prev");
+let next = document.getElementById("next");
+let current = 0;
+let box = document.getElementById("slideShow");
+let isHover = false;
+
+box.onmouseover= ()=>{isHover=true;};
+box.onmouseleave=()=>{isHover=false;};
+
+function showSlides(n) {
+    for (let i = 0; i < slides.length; i++) {
+        slides[i].style.display="none";
+    }
+    slides[n].style.display="block";
+}
+
+function prevSlide(){
+    if(current>0){
+        current-=1;
+    } else{
+        current=slides.length-1;
+    }
+    showSlides(current);
+}
+function nextSlide(){
+    if(current<2){
+        current+=1;
+    } else{
+        current=0;
+    }
+    showSlides(current);
+}
+
+function autoSlide(){
+    if(!isHover)
+        nextSlide();
+    setTimeout(autoSlide,1500);
+}
+
+showSlides(current);
+prev.onclick=prevSlide;
+next.onclick=nextSlide;
+setTimeout(autoSlide,1500);
