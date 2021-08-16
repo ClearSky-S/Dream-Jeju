5adc8cd31aa566fd862c142d2317413d376163ad
Jang Junhyuck <ggogono2@gmail.com>
2021-08-16 14:49:10 +0900

    slideshow

--- js/slideshow.js
+++ js/slideshow.js
@@ -5,40 +5,40 @@ let current = 0;
 let box = document.getElementById("slideShow");
 let isHover = false;
 
-box.onmouseover= ()=>{isHover=true;};
-box.onmouseleave=()=>{isHover=false;};
+box.onmouseover = () => { isHover = true; };
+box.onmouseleave = () => { isHover = false; };
 
 function showSlides(n) {
     for (let i = 0; i < slides.length; i++) {
-        slides[i].style.display="none";
+        slides[i].style.display = "none";
     }
-    slides[n].style.display="block";
+    slides[n].style.display = "block";
 }
 
-function prevSlide(){
-    if(current>0){
-        current-=1;
-    } else{
-        current=slides.length-1;
+function prevSlide() {
+    if (current > 0) {
+        current -= 1;
+    } else {
+        current = slides.length - 1;
     }
     showSlides(current);
 }
-function nextSlide(){
-    if(current<2){
-        current+=1;
-    } else{
-        current=0;
+function nextSlide() {
+    if (current < 2) {
+        current += 1;
+    } else {
+        current = 0;
     }
     showSlides(current);
 }
 
-function autoSlide(){
-    if(!isHover)
+function autoSlide() {
+    if (!isHover)
         nextSlide();
-    setTimeout(autoSlide,1500);
+    setTimeout(autoSlide, 1500);
 }
 
 showSlides(current);
-prev.onclick=prevSlide;
-next.onclick=nextSlide;
-setTimeout(autoSlide,1500);
+prev.onclick = prevSlide;
+next.onclick = nextSlide;
+setTimeout(autoSlide, 1500);
